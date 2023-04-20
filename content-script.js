6d9455931b07ac3210f47bf63316dcc3f83cff34
Eric Ng <eric.ng@digintent.com>
2023-04-20 19:33:19 -0400

    asdasd

--- content-script.js
+++ content-script.js
@@ -15,61 +15,61 @@ let observerOptions = {
     subtree: true,
 };
 
-let changelog = [];
+let currentSnapshot = [];
+let allChanges = [];
+
+function getTextBlocks(currentSnapshot) {
+    currentSnapshot = []
+    let notionTextBlocks = document.querySelectorAll(".notion-text-block");
+    for (let [index, textBlock] of notionTextBlocks.entries()) {
+        currentSnapshot.push(textBlock.innerText);
+    }
+    console.log(currentSnapshot)
+}
 
 function callback(mutationList) {
     mutationList.forEach((mutation) => {
         switch (mutation.type) {
             case "characterData":
                 if (mutation.target.nodeName === "#text") {
-                    console.log("characterData changed (insert text)");
-                    changelog.push(new TextEvent("insert", mutation.target.textContent))
-                    console.log(changelog)
-                    console.log(mutation.target)
+                    console.log("===[LOG] Insert Text===)");
+                    getTextBlocks(currentSnapshot)
+                    allChanges.push(new TextEvent("insertText", mutation.target.textContent))
+                    console.log(allChanges)
                 }
                 break;
             case "childList":
                 if (mutation.target.className === "notion-page-content") {
-                    console.log("add or deleted notion Block")
+                    if (mutation.removedNodes.length > 0) {
+                        console.log("===[LOG] Deleted Notion Block===")
+                        getTextBlocks(currentSnapshot)
+                        allChanges.push(new TextEvent("deleteBlock", mutation.target.textContent))
+                        console.log(allChanges)
+                    } else {
+                        console.log("===[LOG] Inserted Notion Block===")
+                        getTextBlocks(currentSnapshot)
+                        allChanges.push(new TextEvent("insertBlock", mutation.target.textContent))
+                        console.log(allChanges)
+                    }
                 } else if (mutation.target.className === "notranslate") {
-                    console.log("childList changed (delete text)");
-                    console.log(mutation);
-                    changelog.push(new TextEvent("delete", mutation.target.textContent))
-                    console.log(changelog)
+                    console.log("===[LOG] Delete Text===");
+                    getTextBlocks(currentSnapshot)
+                    allChanges.push(new TextEvent("deleteText", mutation.target.textContent))
+                    console.log(allChanges)
                 }
-                break;
-            case "subtree":
-                console.log("subtree triggered");
-                console.log(mutation);
                 break;
         }
     });
 }
 
-let allText = [];
-let allNotionBlockObservers = [];
 
 console.log("Delayed for 3 seconds.");
 setTimeout(() => {
+    console.log("Extension Loaded")
+
     let notionPageContent = document.querySelector(".notion-page-content")
-    console.log("in here")
-    console.log(notionPageContent)
     let notionPageContentObserver = new MutationObserver(callback);
     notionPageContentObserver.observe(notionPageContent, observerOptions);
 }, "3000");
 
 
-function foobar() {
-    let notionTextBlocks = document.querySelectorAll(".notion-text-block");
-    console.log(notionTextBlocks);
-    for (let [index, textBlock] of notionTextBlocks.entries()) {
-        console.log(textBlock);
-        allText.push(textBlock.innerText);
-
-        let notionBlockObserver = new MutationObserver(callback);
-        allNotionBlockObservers.push(notionBlockObserver);
-        notionBlockObserver.observe(notionTextBlocks[index], observerOptions);
-    }
-    console.log(allText)
-    console.log(allNotionBlockObservers);
-}
