43298b03da13f3dea4a29192a1cc8ba5f6529c90
Eric Ng <eric.ng@digintent.com>
2023-04-20 18:14:39 -0400

    asdasd

--- content-script.js
+++ content-script.js
@@ -1,50 +1,75 @@
 console.log("content-script is running on notion");
 
-class Events {
-    constructor(action, index, data) {
+class TextEvent {
+    constructor(action, data, blockIndex) {
         this.action = action;
-        this.index = index;
         this.data = data;
+        this.blockIndex = blockIndex;
     }
 }
 
 let observerOptions = {
     characterData: true,
-    attributes: true,
     childList: true,
+    attributes: true,
     subtree: true,
 };
 
-function callback(mutationList, observer) {
+let changelog = [];
+
+function callback(mutationList) {
     mutationList.forEach((mutation) => {
         switch (mutation.type) {
             case "characterData":
-                console.log("characterData changed");
-                console.log(mutation);
+                if (mutation.target.nodeName === "#text") {
+                    console.log("characterData changed (insert text)");
+                    changelog.push(new TextEvent("insert", mutation.target.textContent))
+                    console.log(changelog)
+                    console.log(mutation.target)
+                }
                 break;
             case "childList":
-                console.log("childList changed");
+                if (mutation.target.className === "notion-page-content") {
+                    console.log("add or deleted notion Block")
+                } else if (mutation.target.className === "notranslate") {
+                    console.log("childList changed (delete text)");
+                    console.log(mutation);
+                    changelog.push(new TextEvent("delete", mutation.target.textContent))
+                    console.log(changelog)
+                }
+                break;
+            case "subtree":
+                console.log("subtree triggered");
                 console.log(mutation);
                 break;
         }
     });
 }
 
-let allText = "";
+let allText = [];
+let allNotionBlockObservers = [];
 
-console.log("Delayed for 5 seconds.");
+console.log("Delayed for 3 seconds.");
 setTimeout(() => {
-    // Try out notion-selectable
-    // let notionTextBlocks = document.querySelectorAll(".notion-selectable");
+    let notionPageContent = document.querySelector(".notion-page-content")
+    console.log("in here")
+    console.log(notionPageContent)
+    let notionPageContentObserver = new MutationObserver(callback);
+    notionPageContentObserver.observe(notionPageContent, observerOptions);
+}, "3000");
 
+
+function foobar() {
     let notionTextBlocks = document.querySelectorAll(".notion-text-block");
     console.log(notionTextBlocks);
-    for (let textBlock of notionTextBlocks) {
+    for (let [index, textBlock] of notionTextBlocks.entries()) {
         console.log(textBlock);
-        allText += textBlock.innerText;
-    }
-    console.log(allText);
+        allText.push(textBlock.innerText);
 
-    let observer = new MutationObserver(callback);
-    observer.observe(notionTextBlocks[notionTextBlocks.length - 1], observerOptions);
-}, "5000");
+        let notionBlockObserver = new MutationObserver(callback);
+        allNotionBlockObservers.push(notionBlockObserver);
+        notionBlockObserver.observe(notionTextBlocks[index], observerOptions);
+    }
+    console.log(allText)
+    console.log(allNotionBlockObservers);
+}
