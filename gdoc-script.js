86d60d30bb488cb71676820d5a629853aee7c6bf
Eric Ng <eric.ng@digintent.com>
2023-05-01 19:52:34 -0400

    can get keyevents

--- gdoc-script.js
+++ gdoc-script.js
@@ -1,6 +1,15 @@
 console.log("running on gdoc");
 
-async function getGoogleDocument(token) {
+let token = "";
+
+class Snapshot {
+    constructor(data, time) {
+        this.data = data;
+        this.time = time;
+    }
+}
+
+async function getGoogleDocument() {
     const response = await fetch(
         "https://docs.googleapis.com/v1/documents/1zaLaPwcU_ZypiadBbp4Js7FFTpywY0E7UEY6Sl48ewc",
         {
@@ -11,13 +20,48 @@ async function getGoogleDocument(token) {
             },
         }
     );
-    console.log(response.json())
+    return response.json();
 }
 
-chrome.storage.local.get(["gdoc"]).then((result) => {
-    console.log("Value currently is " + result.gdoc);
-    let token = result.gdoc;
-    getGoogleDocument(token).catch(error => {
-        console.log(error);
-    })
-});
+function parseBody(bodyContent) {
+    let body = "";
+    bodyContent.forEach((l) => {
+        if (l.paragraph) {
+            l.paragraph.elements.forEach((el) => {
+                body += el.textRun.content;
+            });
+        }
+    });
+    return body;
+}
+
+function createSnapshot() {
+    getGoogleDocument().then((document) => {
+        let body = parseBody(document.body.content);
+        console.log(new Snapshot(body, new Date()));
+    });
+}
+
+function main() {
+    // FIFO
+    let checkCopypaste = [];
+    let editingIFrame = document.querySelector(
+        ".docs-texteventtarget-iframe"
+    );
+    editingIFrame.contentDocument.addEventListener("keydown", function(event) {
+        console.log(event)
+    });
+
+    // document.addEventListener("keypress", function(event) {
+    //     console.log(event)
+    // });
+
+    chrome.storage.local.get(["gdoc"]).then((result) => {
+        console.log("Value currently is " + result.gdoc);
+        token = result.gdoc;
+
+        setInterval(createSnapshot, 5000);
+    });
+}
+
+main();
